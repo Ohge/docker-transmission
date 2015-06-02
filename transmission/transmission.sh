7d7fe68802414fe2a375024c878467d06f3a38e0
Ohge <chris.ohge@gmail.com>
2015-06-02 04:33:01 -0700

    compacted

--- transmission/transmission.sh
+++ transmission/transmission.sh
@@ -2,21 +2,22 @@
 
 ### SET VARIABLES
 USRN=transmission
-HOME_DIR=/home/$USRN
-USR_CONF=$HOME_DIR/settings.json
-DEF_CONF=$HOME_DIR/settings.json.default
+CONF_DIR=/etc/transmission-daemon
+USR_CONF=$CONF_DIR/settings.json
+DEF_CONF=$CONF_DIR/settings.json.default
 
 ### MANAGE CONFIGS
 if [[ ! -f $USR_CONF ]]; then cp $DEF_CONF $USR_CONF; fi
 
 ### MANAGE USER AND VOLUME OWNERSHIP
-if [[ ! $(grep --quiet '$HOME_DIR' /etc/passwd) ]]; then
+if [[ ! $(grep --quiet '$CONF_DIR' /etc/passwd) ]]; then
   ### CREATE APPLICAITON USER
-  echo "$USRN:x:$UID:$GID:$USRN,,,:$HOME_DIR:/bin/bash" >> /etc/passwd
+  echo "$USRN:x:$UID:$GID:$USRN,,,:$CONF_DIR:/bin/bash" >> /etc/passwd
   echo "$USRN:x:$UID:" >> /etc/group
   ### ASSIGN VOLUME OWNERSHIP
-  chown $UID:$GID -R $HOME_DIR
+  chown $UID:$GID -R $CONF_DIR
+  chown $UID:$GID -R /downloads
 fi
 
 ### START SERVICE USING RUNTIME UID AND GID
-exec su - $USRN -c "/usr/bin/transmission-daemon -f -g $HOME_DIR 2>&1" 2>/dev/null
+exec su - $USRN -c "/usr/bin/transmission-daemon -f -g $CONF_DIR 2>&1" 2>/dev/null
