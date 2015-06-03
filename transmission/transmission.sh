00c3126ec1ba5379a0833a0021923de7113f293d
Ohge <chris.ohge@gmail.com>
2015-06-02 23:42:30 -0700

    Fixed fetch dir (might need to be a volume)

--- transmission/transmission.sh
+++ transmission/transmission.sh
@@ -2,6 +2,7 @@
 
 ### SET VARIABLES
 USRN=transmission
+USER_DIR=/downloads
 CONF_DIR=/etc/transmission-daemon
 USR_CONF=$CONF_DIR/settings.json
 DEF_CONF=$CONF_DIR/settings.json.default
@@ -13,11 +14,13 @@ if [[ ! -f $USR_CONF ]]; then cp $DEF_CONF $USR_CONF; fi
 if [[ ! $(grep --quiet '$CONF_DIR' /etc/passwd) ]]; then
   ### CREATE APPLICAITON USER
   echo "$USRN:x:$UID:$GID:$USRN,,,:$CONF_DIR:/bin/bash" >> /etc/passwd
-  echo "$USRN:x:$UID:" >> /etc/group
+  echo "$USRN:x:$GID:" >> /etc/group
+  ### CREATE FETCH DIRECTORY
+  mkdir -p $CONF_DIR/fetch
   ### ASSIGN VOLUME OWNERSHIP
   chown $UID:$GID -R $CONF_DIR
-  chown $UID:$GID -R /downloads
+  chown $UID:$GID -R $USER_DIR
 fi
 
 ### START SERVICE USING RUNTIME UID AND GID
-exec su - $USRN -c "/usr/bin/transmission-daemon -f -g $CONF_DIR 2>&1" 2>/dev/null
+exec su - $USRN -c "/usr/bin/transmission-daemon -f 2>&1" 2>/dev/null
