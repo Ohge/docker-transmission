2ed82a5d85c46350a27a371933ef5f2738811b57
Ohge <chris.ohge@gmail.com>
2015-06-04 03:47:00 -0700

    Compacted

--- install.sh
+++ install.sh
@@ -1,64 +1,52 @@
 #!/bin/bash
 
-### APP VARS
-TYPE=svc
-NAME=transmission
-CFG_DIR=$HOME/.docker-transmission
-CFG_VOL=/etc/transmission-daemon
-USR_DIR=$HOME/downloads
-USR_VOL=/downloads
-GUI_PORT=9091
-P2P_PORT=54321
-
-### RUNTIME VARS
-USR=$(whoami)
-IMG=$TYPE/$NAME
-APP=$USR.$NAME
-LUID=$(id -u $USR)
-LGID=$(id -g $USR)
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+### LOAD COMMON COMPONENTS
+DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+source $DIR/.appvars
+source $DIR/.common
 
 ### CHECK IF CONTAINER ALREADY EXISTS
 CHECK=$(docker ps -a | awk '{print $NF}' | grep $APP)
 if [[ $CHECK == *"$APP"* ]]; then echo -e "$APP already installed for this user\nRun ./uninstall.sh to remove"; exit 1; fi
 
 ### BUILD CONTAINER
-CMDMSG="Installation of image"
+printf "Installation of image $IMG "
 CMDRES=$(docker build -t $IMG $DIR)
-if [ $? -eq 0 ]; then echo "+$CMDMSG $IMG complete"; else echo -e "-$CMDMSG $IMG failed!\n$CMDRES"; exit 1; fi
+if [ $? -eq 0 ]; then echo "complete";
+else echo -e "failed!\n$CMDRES"; exit 1; fi
 
-### MAP CONFIG DIRECTORY
-CMDMSG="Installation of volume"
-CMDRES=$(cp -r $DIR/$NAME $CFG_DIR)
-if [ $? -eq 0 ]; then echo "+$CMDMSG $CFG_DIR complete"; else echo -e "-$CMDMSG $CFG_DIR failed!\n$CMDRES"; exit 1; fi
+### SETUP CONFIG DIRECTORY
+printf "Installation of volume $CONFIG_DIR "
+CMDRES=$(cp -r $DIR/$NAME $CONFIG_DIR)
+if [ $? -eq 0 ]; then echo "complete";
+else echo -e "failed!\n$CMDRES"; exit 1; fi
 
-### MAP DOWNLOAD DIRECTORY
-CMDMSG="Installation of volume"
-CMDRES=$(mkdir -p $USR_DIR)
-if [ $? -eq 0 ]; then echo "+$CMDMSG $USR_DIR complete"; else echo -e "-$CMDMSG $USR_DIR failed!\n$CMDRES"; exit 1; fi
+### SETUP DOWNLOAD DIRECTORY
+printf "Installation of volume $USER_DIR "
+CMDRES=$(mkdir -p $USER_DIR)
+if [ $? -eq 0 ]; then echo "complete";
+else echo -e "failed!\n$CMDRES"; exit 1; fi
 
-### MAP GUI PORT
-CMDMSG="Installation of gui_port"
-LGUI_PORT=$($DIR/.port.sh $GUI_PORT)
-if [ $LGUI_PORT -gt 0 ]; then echo "+$CMDMSG $LGUI_PORT complete"; else echo "-$CMDMSG $LGUI_PORT failed!\n$LGUI_PORT"; exit 1; fi
+### FIND GUI PORT
+LGUI_PORT=$(GetPort $GUI_PORT)
+echo "Installation of gui_port $LGUI_PORT complete";
 
-### MAP P2P PORT
-CMDMSG="Installation of p2p_port"
-LP2P_PORT=$($DIR/.port.sh $P2P_PORT)
-if [ $LP2P_PORT -gt 0 ]; then echo "+$CMDMSG $LP2P_PORT complete"; else echo "-$CMDMSG $LP2P_PORT failed!\n$LP2P_PORT"; exit 1; fi
+### FIND P2P PORT
+LP2P_PORT=$(GetPort $P2P_PORT)
+echo "Installation of p2p_port $LP2P_PORT complete";
 
-### RUN USER CENTRIC CONTAINER
+### RUN APP CONTAINER
 RUN=$(docker run -d \
 --name $APP \
 -e UID=$LUID \
 -e GID=$LGID \
--v $CFG_DIR:$CFG_VOL \
--v $USR_DIR:$USR_VOL \
+-v $CONFIG_DIR:$CONFIG_VOL \
+-v $USER_DIR:$USER_VOL \
 -p $LGUI_PORT:$GUI_PORT/tcp \
 -p $LP2P_PORT:$P2P_PORT/tcp \
 -p $LP2P_PORT:$P2P_PORT/udp \
 $IMG)
 REC=$?
-CMDMSG="Installation of container"
-if [ $REC -eq 0 ]; then echo "+$CMDMSG $APP complete"; else echo "-$CMDMSG $APP failed!"; fi
+printf "Installation of container $APP "
+if [ $REC -eq 0 ]; then echo "complete"; else echo "failed!"; exit 1; fi
 exit $REC
